539db9bf40837b61574835e204a0403c842d0869
Rakesh Chandra <kolaganirakeshchandra@gmail.com>
2020-05-16 13:43:09 +0530

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import { HttpClientModule } from '@angular/common/http';
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
